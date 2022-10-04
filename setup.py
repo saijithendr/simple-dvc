adcc6943a2730cc18e3e3b0cf8edc2069127c935
saijithendr <jithenkumar1@gmail.com>
2022-10-04 13:15:38 +0200

    set up added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src", 
+    version="0.0.1",
+    description="Its a wine quality package", author="saijithendr",
+    packages=find_packages(),
+    license="MIT"
+)
