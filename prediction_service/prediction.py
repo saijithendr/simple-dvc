a736743d72a694b5bdf912e0f216485583815e95
saijithendr <jithenkumar1@gmail.com>
2022-10-08 12:52:28 +0200

    test done

--- prediction_service/prediction.py
+++ prediction_service/prediction.py
@@ -73,6 +73,14 @@ def api_response(dict_req):
             response = predict(data)
             response = {"response": response}
             return response
-    except Exception as e:
+    except NotInRange as e:
         response = {"the expected range":get_schema(), "response": str(e)}
+        return response
+    
+    except NotInCols as e:
+        response = {"the expected cols":get_schema().keys(), "response": str(e)}
+        return response
+    
+    except Exception as e:
+        response = {"response": str(e)}
         return response
